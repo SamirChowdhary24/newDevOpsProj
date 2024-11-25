dbf7427bf15161f013f589f9f3c6fd780f73576b
Samir Chowdhary <143581623+SamirChowdhary24@users.noreply.github.com>
2024-11-25 12:42:00 +0530

    Update script1.sh
    added  comments

--- script1.sh
+++ script1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-DISK_USAGE=$(df -h / | tail -1 | awk '{print $5}' | cut -d'%' -f1)
+DISK_USAGE=$(df -h / | tail -1 | awk '{print $5}' | cut -d'%' -f1) #he final disk usage value is stored in the variable DISK_USAGE
 if [ "$DISK_USAGE" -gt 80 ]; then
     echo "WARNING: Disk usage is above 80% ($DISK_USAGE%)"
     exit 1
 else
     echo "Disk usage is normal: $DISK_USAGE%"
 fi
+#The script monitors disk space and warns if usage exceeds 80%, helping prevent storage issues.
